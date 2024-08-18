64b392ec38c3e1bfb9fe0ce2d78e4e17119ebf50
Alina <micf2f@gmail.com>
2024-08-18 20:15:10 +0300

    First homework - create app, create components, props

---
+++ lesson_1/src/main.jsx
@@ -0,0 +1,21 @@
+import { createRoot } from 'react-dom/client'
+
+
+const Heading = ({color, text}) => {
+  return <h1 style={{color}}> {text} </h1>;
+}
+
+const Description = ({textStyle}) => {
+
+  const name = prompt('Enter your name');  
+  return <h2 style={{fontStyle: textStyle}}> {name}'s first React application. </h2>;
+
+}
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <Heading color="crimson" text="Hello, world!"/>,
+    <Description textStyle="italic"/>
+  </>
+);
+
